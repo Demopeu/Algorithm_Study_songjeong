c00007ba7e3d14be2c999d1844f7d0f2d45787a2
akeroroh <jah000118@naver.com>
2024-04-02 16:49:00 +0900

    week10_Implementation_김동현

---
+++ Implementation/C++/Baekjoon_18111_김동현.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{	
+	ios_base::sync_with_stdio(false);
+	cin.tie(0); cout.tie(0);
+	int N,M,B,floor=0,num=100000000000;
+	cin >> N >> M >> B;
+	int matrix[500][500];
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < M; j++)
+		{
+			int answer;
+			cin >> answer;
+			matrix[i][j] = answer;
+		}
+	}
+	for (int h = 0; h < 257; h++)
+	{
+		int plus = 0, minus = 0;
+		for (int i = 0; i < N; i++)
+		{
+			for (int j = 0; j < M; j++)
+			{
+				//int result;
+				//result = matrix[i][j];
+				if (matrix[i][j]>h)
+				{
+					plus += matrix[i][j] - h;
+				}
+				if (h > matrix[i][j])
+				{
+					minus += h - matrix[i][j];
+				}
+			}
+
+		}
+		if (plus + B >= minus) {
+			if (plus * 2 + minus <= num) {
+				num = plus * 2 + minus;
+				floor = h;
+			}
+		}
+	}
+	cout << num <<" "<< floor << '\n';
+	return 0;
+}
+
+//https://www.acmicpc.net/problem/18111
---
+++ Implementation/C++/Baekjoon_2960_김동현.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	int N, M, answer = 0;
+	cin >> N >> M;
+	bool arr[1001] = { false, false };
+
+	for (int i = 2; i <= N; i++) {
+		arr[i] = true;
+	}
+
+	for (int i = 2; i <= N; i++) {
+		if (arr[i]) {
+			for (int j = i; j <= N; j += i) {
+				if (arr[j]) {
+					arr[j] = false;
+					answer += 1;
+					if (answer == M) {
+						cout << j << '\n';
+					}
+				}
+			}
+		}
+	}
+	return 0;
+}
+//https://www.acmicpc.net/problem/2960
